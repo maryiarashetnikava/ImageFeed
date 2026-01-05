991df9dfdafa915a2e1d48605df96e1151b33b6c
Mary <mariaprohor75@gmail.com>
2026-01-05 12:14:51 +0100

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Maria Reshetnikova on 5.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
