49f71190663642425144c59b150893d3d860ac08
Maryia Rashetnikava <mariaprohor75@gmail.com>
2026-01-20 13:10:10 +0100

    sprint 9 changes (part 1)

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,27 @@
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    @IBOutlet private var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+        
+        print("🔥 SingleImageViewController loaded")
+            print("presentingViewController =", presentingViewController as Any)
+            print("navigationController =", navigationController as Any)
+    }
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
