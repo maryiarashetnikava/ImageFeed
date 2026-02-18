1df62d03f71c16d24a5ca35cc1ce3213c7626e1f
Maryia Rashetnikava <mariaprohor75@gmail.com>
2026-02-18 12:13:14 +0100

    task sprint 11 part 2

---
+++ ImageFeed/Service/OAuth2/OAuth2TokenStorage.swift
@@ -0,0 +1,19 @@
+import Foundation
+import SwiftKeychainWrapper
+
+final class OAuth2TokenStorage {
+    private let tokenKey = "OAuth2Token"
+    
+    var token: String? {
+        get {
+            return KeychainWrapper.standard.string(forKey: tokenKey)
+        }
+        set {
+            if let token = newValue {
+                KeychainWrapper.standard.set(token, forKey: tokenKey)
+            } else {
+                KeychainWrapper.standard.removeObject(forKey: tokenKey)
+            }
+        }
+    }
+}
